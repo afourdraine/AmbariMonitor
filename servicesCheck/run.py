7617f99a3dbd2945b62809d37de55bca6c6c7dea
Alexandre Fourdraine <alexandre.fourdraine@gmail.com>
2018-11-02 15:36:27 +0100

    Services check moniroting done v1

---
+++ servicesCheck/run.py
@@ -0,0 +1,52 @@
+from servicesCheck.var import *
+from servicesCheck.funtion import *
+
+serviceCheck = runServicesCheck(nodeName, clusterName, auth_values)
+
+batchId = serviceCheck["resources"][0]["RequestSchedule"]["id"]
+batchId = str(batchId)
+batchStatus = getBachStatus(nodeName, clusterName, auth_values, batchId)
+
+print(batchStatus)
+while batchStatus == "SCHEDULED":
+    batchStatus = getBachStatus(nodeName, clusterName, auth_values, batchId)
+
+    parse = getServicesCheck(nodeName, clusterName, auth_values)
+
+    final = {}
+    failed = {}
+    failed_test = 0
+    total_test = 0
+
+    for item in parse["items"]:
+
+        if item["Requests"]["request_status"] == "COMPLETED" and item["Requests"]["request_context"] not in final:
+
+            final.update({item["Requests"]["request_context"] : item["Requests"]["request_status"]})
+
+            total_test += 1
+
+        elif item["Requests"]["request_status"] == "FAILED" and item["Requests"]["request_context"] not in final:
+
+            final.update({item["Requests"]["request_context"]: item["Requests"]["request_status"]})
+
+            failed.update({item["Requests"]["request_context"]: item["Requests"]["request_status"]})
+
+            failed_test += 1
+
+            total_test += 1
+
+if failed:
+    print("Number of Services Checked :" + str(total_test))
+    print("Number of Services Check failed : " + str(failed_test))
+    print(failed)
+else:
+    print("Number of Services Checked :" + str(total_test))
+    print("Number of Services Check failed : " + str(failed_test))
+    print(final)
+
+
+
+
+
+
---
+++ servicesCheck/var.py
@@ -0,0 +1,5 @@
+user = "admin"
+passwd = "afourdraine"
+nodeName = 'c154-node1'
+clusterName = 'c154'
+auth_values = (user, passwd)
