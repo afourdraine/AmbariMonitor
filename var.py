c4b9edbacd6132bd57d5133d0688e2b76fbad232
Alexandre Fourdraine <alexandre.fourdraine@gmail.com>
2018-12-21 11:40:42 +0100

    remove default var

--- var.py
+++ var.py
@@ -1,20 +1,20 @@
 # ambari user with necessary rights to request services and alerts status
-user = "admin"
+user = ""
 
 # user password
-passwd = "afourdraine"
+passwd = ""
 
 # resolvable name of the Ambari server
-nodeName = "c154-node1"
+nodeName = ""
 
 # name of the cluster managed by Ambari
-clusterName = "c154"
+clusterName = ""
 
 # the port where Ambari is listening
-port = "8080"
+port = ""
 
 # if Ambari is configured in https put "y" if not, put "n"
-https = "n"
+https = ""
 
 # by default verify is set to false. you can set to the CA_BUNDLE path to avoid warnings
 verify = False
